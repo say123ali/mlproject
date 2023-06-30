0afde8a13e96995865e04bac47183df02319f9fb
say123ali <sayali.sk333@gmail.com>
2023-06-30 11:24:18 +0530

    Data Ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,8 +3,14 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 
 @dataclass
 
@@ -34,6 +40,9 @@ class DataIngestion:
         
         except Exception as e:
             raise CustomException(e,sys)
-if __name__=='__main__':
+if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
